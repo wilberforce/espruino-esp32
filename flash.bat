93827bb9e9760df39686da2ceb628349c777687e
Rhys.Williams <github@wilberfoce.co.nz>
2017-01-12 21:25:18 +1300

    Allow for window 10 USB bug

--- flash.bat
+++ flash.bat
@@ -1,3 +1,3 @@
-..\esp-idf\components\esptool_py\esptool\esptool.py  --chip esp32 --port COM3 --baud 921600 --before default_reset --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x10000 ../Espruino/espruino_esp32.bin
+..\esp-idf\components\esptool_py\esptool\esptool.py  --chip esp32 --port COM3 --baud 921600 --before esp32r0 --after hard_reset write_flash -z --flash_mode dio --flash_freq 40m --flash_size detect 0x10000 ../Espruino/espruino_esp32.bin
 pause
 
