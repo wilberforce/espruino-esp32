b8dae0808c93819efcc338cf4f835a1995194fe5
wilberforce <github@wilberforce.co.nz>
2017-01-14 10:04:52 +1300

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -2,6 +2,6 @@
 export ESP_IDF_PATH=$(pwd)/esp-idf
 export IDF_PATH=$(pwd)/esp-idf
 export ESP_APP_TEMPLATE_PATH=$(pwd)/app
-export ESP32_BOARD=1
+export ESP32=1
 #export RELEASE=1
 [[ ":$PATH:" != *":/opt/xtensa-esp32-elf/bin:"* ]] && PATH="/opt/xtensa-esp32-elf/bin:${PATH}"
